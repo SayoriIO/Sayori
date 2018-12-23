d18842c310c5c164a44224abb0f28e85bfaf118d
Capuccino <chinodesuuu@gmail.com>
2018-12-23 05:54:03 +0000

    uwu
    Signed-off-by: Capuccino <chinodesuuu@gmail.com>

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ import asyncio
 import yaml
 import shutil
 import hashlib
-<<<<<<< HEAD
 import pickle
 import redis
-=======
->>>>>>> master
+
 
 from io import BytesIO, StringIO
 from aiohttp import web
@@ -91,11 +89,7 @@ def gen_img(poem, font, bg):
         bg = bg.resize((bg.width, height), Image.BICUBIC)
         draw = ImageDraw.Draw(bg)
 
-<<<<<<< HEAD
-    draw.text((PADDING, PADDING), poem, '#000000', font)
-=======
     draw.text((PADDING-OFFSET , PADDING), poem, '#000000', font)
->>>>>>> master
     bg.save(b, 'png')
     b.seek(0)
 
@@ -148,7 +142,6 @@ async def handle_request(req):
     _font = body.get('font', DEFAULT_FONT)
     hashed = hashlib.md5((body['poem'] + _font).encode('utf8')).hexdigest()
     hashed_path = f'./poems/{hashed}.png'
-<<<<<<< HEAD
     
     # Save to redis
     if CACHE:
@@ -159,12 +152,6 @@ async def handle_request(req):
 
     if os.path.exists(hashed_path) and CACHE:
         res_url = f'{RESULT_URL}/poems/{hashed}.png'
-=======
-
-    if os.path.exists(hashed_path) and CACHE:
-        res_url = f'{RESULT_URL}/poems/{hashed}.png'
-
->>>>>>> master
         return web.json_response({'id': hashed, 'url': res_url})
 
     bg = BACKGROUNDS.get(_font, DEFAULT_BG).copy()
@@ -204,10 +191,7 @@ if not os.path.exists('./config.yaml'):
             'default_font': os.environ['DEFAULT_FONT'],
             'default_bg': os.environ['DEFAULT_BG'],
             'cdn': os.environ['CDN'],
-<<<<<<< HEAD
             'redis_host': os.environ['REDIS_URL'],
-=======
->>>>>>> master
             'result_url': os.environ['RESULT_URL'],
             'cache': True if os.environ['CACHE'].lower() == 'true' else False,
             'port': int(os.environ['PORT'])
@@ -221,12 +205,8 @@ if os.path.exists('./config.yaml'):
     with open('./config.yaml') as c:
         config = yaml.load(c)
 
-<<<<<<< HEAD
-PADDING = config['padding']  # px
-=======
 PADDING = config['padding'] # px
 OFFSET = PADDING // 4
->>>>>>> master
 DEFAULT_FONT = config['default_font']
 DEFAULT_BG = Image.open('./backgrounds/' + config['default_bg'])
 
