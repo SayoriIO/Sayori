fb2d8ae0fe5ec5cbf92832af4936bd506bf2e1f9
Ayase Minori <ayane@vignetteapp.org>
2023-03-26 22:35:49 +0000

    Force ASCII on requests
    Signed-off-by: GitHub <noreply@github.com>

--- webserver_api.py
+++ webserver_api.py
@@ -11,9 +11,9 @@
 # Requests can either be a GET or a POST, with the former being allowed to support browsers.
 # Both methods of submitting data are supported with both methods,
 # however most things that send a GET will only allow the query string method (as far as I am aware).
-import image
 import os
 import hashlib
+import image
 import redis
 import gzip
 import logging
@@ -26,22 +26,25 @@ from flask import Flask, Request, Response, request
 CACHE = redis.StrictRedis(host=os.environ.get('REDIS_URL') or 'localhost')
 app = Flask(__name__)
 
+# FIXME: We get UnicodeDecodeError for every request, we will force ASCII to prevent some errors
+app.config['JSON_AS_ASCII'] = False
+
+
 @app.route('/', methods=['GET'])
-def handle_request(req: Request = request):
+async def handle_request(req: Request = request):
     body = req.args.to_dict()
 
     if 'poem' not in body:
-        return Response(json.dumps({'error': 'Missing required field: "poem".', 'code': 1}), status=400)
+        return Response('Missing required field: poem', status=400)
 
     if not isinstance(body['poem'], str):
-        return Response(json.dumps({'error': 'Field "poem" is not a string.', 'code': 2}), status=400)
+        return Response("Field 'poem' is not a string.", status=400)
 
     if not body['poem']:
-        return Response(json.dumps({'error': 'Field "poem" is empty.', 'code': 3}), status=400)
+        return Response("Field 'poem' is empty.", status=400)
 
     if 'font' in body and body['font'] not in image.FONTS:
-        return Response(json.dumps({'error': 'Unsupported font.', 'valid_fonts': image.FONTS.keys(), 'code': 4}), status=400)
-
+        return Response(f'Invalid font, valid fonts are {list(image.FONTS.keys())}', status=400)
 
     poem = body['poem']
     font = body.get('font', image.DEFAULT_FONT)
@@ -52,7 +55,7 @@ def handle_request(req: Request = request):
     # Check if the image is cached
     if CACHE.exists(hash):
         # We already generated this before, so just grab it from Redis
-        data = gzip.decompress(CACHE.get(hash)) # type: ignore
+        data = gzip.decompress(CACHE.get(hash))  # type: ignore
         im = BytesIO(data)
 
         return Response(im.getvalue(), status=200, content_type='image/png')
@@ -63,9 +66,13 @@ def handle_request(req: Request = request):
 
         return Response(im.getvalue(), status=200, content_type='image/png')
 
-logging.basicConfig(format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 if __name__ == '__main__':
-    app.logger.info(f"Starting server on port {os.environ.get('PORT') or 7270}")
-    app.logger.info(f"Using Redis at {os.environ.get('REDIS_URL') or 'redis://localhost:6379'}")
+    app.logger.info(
+        f"Starting server on port {os.environ.get('PORT') or 7270}")
+    app.logger.info(
+        f"Using Redis at {os.environ.get('REDIS_URL') or 'redis://localhost:6379'}")
     app.run(port=int(os.environ.get('PORT') or 7270))
